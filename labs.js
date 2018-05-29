ba2abbbe06262d74a2c536ac225363032a274b9c
CubeRikkert <ricksamuel@gmail.com>
2018-05-29 09:15:42 +0200

    add exercise file

--- labs.js
+++ labs.js
@@ -2,7 +2,7 @@ let age = window.prompt('What\'s your age in years?');
 let gender = window.prompt('what\'s your gender? (m/f)');
 let height = window.prompt('what\s your height in centimeters?');
 let weight = window.prompt('what\'s your weight in kilograms?');
-let activityLevel = window.prompt('\n Please note your activity level on this scale;\n 1. little of no exercise \n 2. exercise/sports 1-3 times a week \n 3. exercise/sports 4-5 times a week \n 4. exercise/sports 6-7 times a week \n 5. very hard exercise/sports of physical job');
+let exerciseLevel = window.prompt('\n Please note your activity level on this scale;\n 1. little of no exercise \n 2. exercise/sports 1-3 times a week \n 3. exercise/sports 4-5 times a week \n 4. exercise/sports 6-7 times a week \n 5. very hard exercise/sports of physical job');
 
 let heightInCM = height / 100
 let bodyWeightSquared = Math.pow(heightInCM,2);
@@ -48,3 +48,5 @@ if (weight > idealWeight) {
   dietCaloriesPerDay = caloriesPerDay + 500
   dietWeeks = (idealWeight - weight) / 0.5
 }
+
+document.write(`Your BMI is ${bmi} .\n At your hight, ideal body weight is ${idealWeight}.\n You need to at ${dietCaloriesPerDay} to reach your ideal weight in ${dietWeeks}.\n In order for you to maintain your ideal weight after you've got there, you need to eat ${caloriesPerDay} calories per day. `);
